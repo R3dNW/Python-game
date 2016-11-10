2bfb3278c3b2cbcaa29a58079482ece74ddba0e6
Joeking360 <rdkilby@Dympna-Kilbys-iMac-4.local>
2016-11-10 21:16:29 +0000

    Makes the game playable (but unwinnable)
    Fixes the looping system, adds item drops, makes use of both
    inventories (nothing yet to put in the passive inventory) and puts
    things in functions to make them easier to handle.

--- Classes practice.py
+++ Classes practice.py
@@ -1,13 +1,39 @@
 #Github copy
 import random
 
-Floor = 0
 class Item:
     name = ""
     description = ""
     effect_type = ""
     boost_type = ""
     potency = 0
+
+Potion_file = open("Items list.txt","r")
+Potion = Potion_file.read()
+Potion_file.close()
+Potion = Potion.split("\n")
+Potion_file = open("Items list.txt","r")
+
+items = []
+
+for line in range(0,len(Potion)):
+    item = Item()
+    potions = Potion_file.readline()
+    potions = potions.strip("\n")
+    potions = potions.split(";")
+    item.name = potions[0]
+    item.description = potions[1]
+    item.effect_type = potions[2]
+    item.boost_type = potions[3]
+    item.potency = int(potions[4])
+    items.append(item)
+
+
+
+
+
+Floor = 0
+
 
 
 class Party:
@@ -41,25 +67,6 @@ class Party:
     def item(self):
         counter = 0
 
-        Potion_file = open("Items list.txt","r")
-        Potion = Potion_file.read()
-        Potion_file.close()
-        Potion = Potion.split("\n")
-        Potion_file = open("Items list.txt","r")
-
-        items = []
-
-        for line in range(0,len(Potion)):
-            item = Item()
-            potions = Potion_file.readline()
-            potions = potions.strip("\n")
-            potions = potions.split(";")
-            item.name = potions[0]
-            item.description = potions[1]
-            item.effect_type = potions[2]
-            item.boost_type = potions[3]
-            item.potency = int(potions[4])
-            items.append(item)
 
 
         b = 1
@@ -80,27 +87,27 @@ class Party:
             else:
                 c = 0
 
-            for i in range(0,len(items)):
+            for i in range(0,len(self.active_inventory)):
 
 
                 if i == counter:
-                    print("--" + items[i].name)
-                    a = items[i].description
+                    print("--" + self.active_inventory[i].name)
+                    a = self.active_inventory[i].description
                     print(a)
 
                 else:
-                    print(items[i].name)
+                    print(self.active_inventory[i].name)
                 print()
 
-        print("You selected: " + items[counter].name)
-        print("            " + items[counter].description)
+        print("You selected: " + self.active_inventory[counter].name)
+        print("            " + self.active_inventory[counter].description)
 
-        if items[counter].boost_type == "Health":
-            self.life_bonus = self.life_bonus + items[counter].potency
-        elif items[counter].boost_type == "Defence":
-            self.defence_bonus = self.defence_bonus + items[counter].potency
-        elif items[counter].boost_type == "Strength":
-            self.strength_bonus = self.strength_bonus + items[counter].potency
+        if self.active_inventory[counter].boost_type == "Health":
+            self.life_bonus = self.life_bonus + self.active_inventory[counter].potency
+        elif self.active_inventory[counter].boost_type == "Defence":
+            self.defence_bonus = self.defence_bonus + self.active_inventory[counter].potency
+        elif self.active_inventory[counter].boost_type == "Strength":
+            self.strength_bonus = self.strength_bonus + self.active_inventory[counter].potency
 
 
     def stats(self):
@@ -115,8 +122,12 @@ class Enemy:
     life = level*3
     strength = level*3
     defence = level*2
+    item = []
 
-
+    def __init__(self):
+        chooser = random.randint(0,self.level)
+        self.item = items[chooser]
+        
 
     def attack(self):
         victim = random.randint(0,partylist.len())
@@ -166,59 +177,75 @@ create_enemy()
 
 
 
+def choice_A(i):
+    damage = i.strength
+    for i in range(0,len(enemy_list)):
+        enemy_list[i].name = names_list[i]
+        print (enemy_list[i].name + "[" + str(i) + "]" )
+        print ("Life: " + str(enemy_list[i].life))
+    victim = int(input("Who do you want to attack? "))
 
-def battle():
-    while len(party_list) > 0 and len(enemy_list) > 0:
-        for i in party_list:
-            i.stats()
-            action = input("""What will you do?
+    print(enemy_list[victim].life)
+    print("defence = " + str(enemy_list[victim].defence) + "and attack power = " + str(damage))
+    if enemy_list[victim].defence > int(damage):
+        print("Attack too weak, 0 damage!")
+        print(enemy_list[victim].life)
+    else:
+        enemy_list[victim].life = enemy_list[victim].life - (damage - enemy_list[victim].defence)
+        print(enemy_list[victim].life)
+        if enemy_list[victim].life <= 0:
+            if enemy.list[victim].item.effect_type == "A":               
+                i.active_inventory.append(enemy_list[victim].item)
+            else:
+                i.passive_inventory.append(enemy_list[victim].item)
+            print("The enemy dropped a: " + enemy_list[victim].item.name + " and " + i.name + " picked it up.")
+            enemy_list.pop(victim)
+            print(len(enemy_list))
+            
+        else:
+            print(enemy_list[victim].life)
+
+
+def choice(i):
+    action = input("""What will you do?
         Attack
         Defend
         Item
         Run(15%)
         """)
-            action = action.upper()
-            if action == "A" or action == "ATTACK":
-                i.stats()
-                damage = i.strength
-                for i in range(0,len(enemy_list)):
-                    enemy_list[i].name = names_list[i]
-                    print (enemy_list[i].name + "[" + str(i) + "]" )
-                    print ("Life: " + str(enemy_list[i].life))
-                victim = int(input("Who do you want to attack? "))
+    action = action.upper()
+    if action == "A" or action == "ATTACK":
+        choice_A(i)
+        
 
-                print(enemy_list[victim].life)
-                print("defence = " + str(enemy_list[victim].defence) + "and attack power = " + str(damage))
-                if enemy_list[victim].defence > int(damage):
-                    print("Attack too weak, 0 damage!")
-                    print(enemy_list[victim].life)
-                else:
-                    enemy_list[victim].life = enemy_list[victim].life - (damage - enemy_list[victim].defence)
-                    print(enemy_list[victim].life)
-                    if enemy_list[victim].life <= 0:
-                        enemy_list.pop(victim)
-                        print(len(enemy_list))
-                        if len(enemy_list) <= 0 or len(party_list) <= 0:
-                            break
-                    else:
-                        print(enemy_list[victim].life)
+    elif action == "D" or action == "DEFENCE":
+        i.defence = i.defence*2
 
-            elif action == "D" or action == "DEFENCE":
-                i.defence = i.defence*2
+    elif action == "I" or action == "ITEM":
+        if len(i.inventory) == 0:
+            print("You have no items")
+            choice(i)
+        else:
+            print(i.life)
+            i.item()
+            print(i.life)
+            choice(i)
 
-            elif action == "I" or action == "ITEM":
-                print(i.life)
-                i.item()
-                i.stats()
-                print(i.life)
 
+def battle():
+    while len(party_list) > 0 and len(enemy_list) > 0:
+        for i in party_list:
+            i.stats()
+            choice(i)
+            if len(enemy_list) <= 0 or len(party_list) <= 0:
+                break
         if len(enemy_list) <= 0 or len(party_list) <= 0:
             break
 
 
         for i in enemy_list:
             damage = i.strength
-            victim = random.randint(0,len(party_list))
+            victim = random.randint(0,(len(party_list)-1))
             print(party_list[victim].name)
             print(party_list[victim].life)
             if party_list[victim].defence > damage:
@@ -229,15 +256,16 @@ def battle():
                 print(party_list[victim].life)
                 if party_list[victim].life <= 0:
                     party_list.pop(victim)
-                    if len(enemy_list) <= 0 or len(enemy_list):
+                    if len(enemy_list) <= 0 or len(party_list) <= 0:                        
                         break
                 else:
-                    print(party_list[victim.life])
+                    print(party_list[victim].life)
 
-        if len(enemy_list) <= 0 or len(enemy_list):
+        if len(enemy_list) <= 0 or len(party_list) <= 0:
             break
         else:
             print()
+            #battle()
 
             
 
